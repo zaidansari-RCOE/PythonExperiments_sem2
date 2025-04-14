8a8102af9cae8ffc1dcf5ef61c36eb6513261e6e
Mohd Zaid Ansari <zaidansari@eng.rizvi.edu.in>
2025-04-14 22:05:00 +0530

    Create expt1.py

---
+++ expt1.py
@@ -0,0 +1,11 @@
+''' Title:Personalized Greeting Generator
+    Name:Md. Zaid Mashooque Ansari
+    Division:C
+    UIN:241P057
+    Roll no:51'''
+    
+
+name = input("Enter your name: ")
+print("Hello, " + name + "! Nice to meet you.")
+
+
