f5e093fffe186eed9e99d0c66be859022b9d2ff3
Mohd Zaid Ansari <zaidansari@eng.rizvi.edu.in>
2025-04-14 22:35:34 +0530

    Create expt9.py

---
+++ expt9.py
@@ -0,0 +1,31 @@
+'''
+Title: Factorial Generator
+Name: Md. Zaid Mashooque Ansari
+Division: C
+UIN: 241P057
+Roll no: 51
+'''
+
+import math
+
+def factorial(n):
+    return math.factorial(n)
+
+# Take input from the user
+num = int(input("Enter number you want to calculate factorial of\n"))
+
+# Print the factorial of the number
+print("Factorial of", num, "is", factorial(num))
+
+"""
+Sample Output:
+
+Enter number you want to calculate factorial of
+5
+Factorial of 5 is 120
+
+Enter number you want to calculate factorial of
+7
+Factorial of 7 is 5040
+"""
+
